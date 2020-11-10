c67515c7224c0bef749264b174d137091d4754a1
Pavlo Hrab <pavlo.hrab@lnu.edu.ua>
2020-11-10 19:52:33 +0200

    Added number of dimensions in the genomicratioset object description

--- R/genomicratioset.R
+++ R/genomicratioset.R
@@ -7,7 +7,9 @@
 #' Only rows for CpG_4, _5, _6, _7, _8 are chosen
 #' with \code{rownames(Locations)[3000:3004]} in order to simulate
 #' real hit with continous CpG island methylation difference.
-#'   
+#' 
+#' Dimensions: The dataset consist of 20 rows (representing CpG islands)
+#' and 5 columns (representing samples)
 #'
 #' @usage data("genomicratioset")
 #' @return A \code{GenomicRatioSet} object.
