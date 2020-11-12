97e807cb5e91cbc302a9e7accbb9fe43eacd6aa8
Leire Abarrategui Martinez <nla94@newcastle.ac.uk>
2020-11-12 10:45:45 +0000

    changes in manova

--- R/EpiMLM.R
+++ R/EpiMLM.R
@@ -1,11 +1,10 @@
-#' @export
+#'
+#'  @export
 epiMLM<-function(beta.values, model)
 {
   #select "model" variable columns (unique(model[,i])!= 1)
-  
- 
   mod <- mlm::mlm(beta.values ~ model[,2])
-  p.value<-mod$aov.tab[1, "Pr(>F)"]
+  statistics<-mod$aov.tab[1, c("Pr(>F)", "R2", "Pr(>F)")]
   
-  return(p.value)
+  return(statistics)
 }
