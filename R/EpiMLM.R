27750979e72e7145f8dddd5cdaf37a88774dafdf
Leire Abarrategui Martinez <nla94@newcastle.ac.uk>
2020-11-12 11:01:02 +0000

    .

--- R/EpiMLM.R
+++ R/EpiMLM.R
@@ -4,7 +4,7 @@ epiMLM<-function(beta.values, model)
 {
   #select "model" variable columns (unique(model[,i])!= 1)
   mod <- mlm::mlm(beta.values ~ model[,2])
-  statistics<-mod$aov.tab[1, c("Pr(>F)", "R2", "Pr(>F)")]
+  output<-mod$aov.tab[1, c("Pr(>F)", "R2", "Pr(>F)")]
   
-  return(statistics)
+  return(output)
 }
